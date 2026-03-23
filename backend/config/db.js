574edc0bbca0e692055f48cb01d8c7aee3d44287
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-23 12:26:36 -0300

    Configurando variables de entorno para mongodb, y seguridad con .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import dns from "node:dns"; // 1. Importamos el módulo de red de Node
+
+// 2. Le decimos a Node que use Google solo para este proyecto
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const conectarDB = async () => {
     try {
-        const db = await mongoose.connect("mongodb://faculedesmabertalot_db_user:hk9f7cyX72jy6egH@cluster0.ecz8ulx.mongodb.net/?appName=Cluster0"
-        );
-
-
+        const db = await mongoose.connect(process.env.MONGO_URI);
         const url = `${db.connection.host}:${db.connection.port}`;
         console.log(`MongoDB conectado en: ${url}`);
     } catch (error) {
