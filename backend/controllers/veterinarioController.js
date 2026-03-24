ad423d851c4e42b3fcbaec26f4b6b704bf3cbb87
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-24 16:00:45 -0300

    Prevenir registros de usuarios duplicados

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,7 +1,15 @@
 import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
-    // const { nombre, password, email } = req.body;
+    const { email } = req.body;
+
+    // Prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne({ email });
+    if (existeUsuario) {
+        const error = new Error('Usuario Ya Registrado');
+        return res.status(400).json({msg: error.message});
+    }
+    
 
     try {
         // Guardar un nuevo veterinario
