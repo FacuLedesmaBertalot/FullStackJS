3f16f95a96b8efe389d307483acf0f8a926bbaf0
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-24 16:44:40 -0300

    Encontrar el usuario a confirmar, validar y volviendo null el token

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -27,8 +27,27 @@ const perfil = (req, res) => {
     res.json({msg: "Mostrando Perfil"});
 };
 
-const confirmar = (req, res) => {
-    res.json({msg: "Confirmando Cuenta"});
+const confirmar = async (req, res) => {
+    const { token } = req.params;
+
+    const usuarioConfirmar = await Veterinario.findOne({token});
+
+    if (!usuarioConfirmar) {
+        const error = new Error('Token no Válido');
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+
+        res.json({msg: "Usuario Confirmado Correctamente"});        
+    } catch (error) {
+        console.log(error);
+    }
+
+
 };
 
 export { registrar, perfil, confirmar };
