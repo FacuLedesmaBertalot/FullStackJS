8d6aab813cdc64d1ce8e96e2f7831e5f5862b526
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-24 10:07:23 -0300

    Leyendo los datos a traves de la API con Postman

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,4 +1,9 @@
 const registrar = (req, res) => {
+    const { nombre, email, password } = req.body;
+
+    console.log(nombre);
+    console.log(email);
+    console.log(password);
     res.json({msg: "Registrando Usuario..."});
 };
 
