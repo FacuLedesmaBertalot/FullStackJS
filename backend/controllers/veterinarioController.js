30e40b1f5b0368d0cec432db509e7244ed19ef4f
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-24 10:31:06 -0300

    Almacenando registros en la bd

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,10 +1,18 @@
-const registrar = (req, res) => {
-    const { nombre, email, password } = req.body;
+import Veterinario from "../models/Veterinario.js";
 
-    console.log(nombre);
-    console.log(email);
-    console.log(password);
-    res.json({msg: "Registrando Usuario..."});
+const registrar = async (req, res) => {
+    // const { nombre, password, email } = req.body;
+
+    try {
+        // Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+
+        res.json({veterinarioGuardado});
+    } catch (error) {
+        console.log(error);
+    }
+
 };
 
 const perfil = (req, res) => {
