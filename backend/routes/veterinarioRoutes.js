8acd18fb83d4f5878cfa0763b32a422bd9b09b01
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-25 17:20:32 -0300

    Creando el routing para resetear password

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,12 +1,18 @@
 import express from 'express';
 const router = express.Router();
-import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js';
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from '../controllers/veterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 
+// Área Pública
 router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+// router.get('/olvide-password/:token', comprobarToken); esto es lo mismo que lo de abajo
+// router.post('/olvide-password/:token', nuevoPassword); esto es lo mismo que lo de abajo
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
+// Área Privada
 router.get('/perfil', checkAuth, perfil);
 
 
