bd43db3117767a0b955c76556db3f7bdc040d51e
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-03-26 13:09:03 -0300

    Enviando la peticion para crear pacientes

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -1,5 +1,15 @@
+import Paciente from "../models/Paciente.js";
 
-const agregarPaciente = (req, res) => {}
+
+const agregarPaciente = (req, res) => {
+    const paciente = new Paciente(req.body);
+    
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
 const obtenerPacientes = (req, res) => {}
 
 export { agregarPaciente, obtenerPacientes };
