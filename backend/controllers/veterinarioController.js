8bbd51efcbf16d4618562b09f96d965c3c6a85b8
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-04 11:40:09 -0300

    Comprobando todo el flujo de autenticacion

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -83,7 +83,12 @@ const autenticar = async (req, res) => {
     // Revisar el password
     if (await usuario.comprobarPassword(password)) {
         // Autenticar
-        res.json({ token: generarJWT(usuario.id) });
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario.id),
+        });
     } else {
         const error = new Error("La Contraseña es Incorrecta");
         return res.status(404).json({msg: error.message});
