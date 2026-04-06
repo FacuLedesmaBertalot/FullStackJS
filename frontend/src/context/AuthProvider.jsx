d42b0e2928ed8a37b2f34edb9b2df6fd72fbbf1d
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-04-06 11:37:14 -0300

    Validando cuando el usuario desea cambiar su password

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -70,6 +70,10 @@ const AuthProvider = ({children}) => {
         }
     }
 
+    const guardarPassword = async (datos) => {
+
+    }
+
     return (
         <AuthContext.Provider
             value={{
@@ -77,7 +81,8 @@ const AuthProvider = ({children}) => {
                 setAuth,
                 cargando,
                 cerrarSesion,
-                actualizarPerfil
+                actualizarPerfil,
+                guardarPassword
             }}
         >
             {children}
--- frontend/src/paginas/CambiarPassword.jsx
+++ frontend/src/paginas/CambiarPassword.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react';
 import AdminNav from "../components/AdminNav";
 import Alerta from "../components/Alerta";
+import useAuth from '../hooks/useAuth';
 
 const CambiarPassword = () => {
 
+    const { guardarPassword } = useAuth();
+
     const [alerta, setAlerta] = useState({});
+    const [password, setPassword] = useState({
+        pwd_actual: '',
+        pwd_nuevo: ''
+    });
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        if (Object.values(password).some(campo => campo === '')) {
+            setAlerta({
+                msg: 'Todos los Campos son Obligatorios',
+                error: true
+            })
+            return;
+        }
+
+        if ( password.pwd_nuevo.length < 6) {
+            setAlerta({
+                msg: 'La Contraseña debe tener Mínimo 6 caracteres',
+                error: true
+            })  
+            return;
+        }
+        
+        guardarPassword(password);
     }
 
     const { msg } = alerta;
@@ -29,20 +54,28 @@ const CambiarPassword = () => {
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Contraseña Actual</label>
                         <input 
-                            type="text"
+                            type="password"
                             className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                            name="nombre"
+                            name="pwd_actual"
                             placeholder='Escribe tu Contraseña Actual'
+                            onChange={e => setPassword({
+                                ...password,
+                                [e.target.name] : e.target.value
+                            })}
                         />
                     </div>
 
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Contraseña Nueva</label>
                         <input 
-                            type="text"
+                            type="password"
                             className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
-                            name="nombre"
+                            name="pwd_nuevo"
                             placeholder='Escribe tu Contraseña Nueva'
+                            onChange={e => setPassword({
+                                ...password,
+                                [e.target.name] : e.target.value
+                            })}
                         />
                     </div>
 
